5e415edd8c1312b47292de5bc42b4c20ca350c0f
kajal764 <kajaldw666@gmail.com>
2020-10-06 12:57:14 +0530

    [Kajal] Added: Registration form and field validation

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+  }
+
+  hide = true;
+  namePattern = '^[A-Za-z]{2,}$';
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  passwordPattern = '^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%]).{6,})$';
+
+
+  registerForm = this.formBuilder.group({
+    firstname: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+    lastname: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    password: ['', [Validators.required, Validators.minLength(6), Validators.pattern(this.passwordPattern)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6), Validators.pattern(this.passwordPattern)]]
+  });
 
   ngOnInit(): void {
   }
 
+  get firstname() {
+    return this.registerForm.get('firstname');
+  }
+
+  get lastname() {
+    return this.registerForm.get('lastname');
+  }
+
+  // tslint:disable-next-line:typedef
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  // tslint:disable-next-line:typedef
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  // tslint:disable-next-line:typedef
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  // tslint:disable-next-line:typedef
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+
+  onSubmit() {
+    
+  }
 }
